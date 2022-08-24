ddd807091af6d109ad064d008d2142803395b67c
Brian Dwyer <brian.dwyer@broadridge.com>
2022-08-24 14:09:44 -0400

    Cleanup user_key

--- internal/provider/resource_user_key.go
+++ internal/provider/resource_user_key.go
@@ -1,6 +1,11 @@
 package provider
 
 import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/go-cty/cty"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
 	chefc "github.com/go-chef/chef"
@@ -8,10 +13,10 @@ import (
 
 func resourceChefUserKey() *schema.Resource {
 	return &schema.Resource{
-		Create: CreateUserKey,
-		Update: UpdateUserKey,
-		Read:   ReadUserKey,
-		Delete: DeleteUserKey,
+		CreateContext: CreateUserKey,
+		UpdateContext: UpdateUserKey,
+		ReadContext:   ReadUserKey,
+		DeleteContext: DeleteUserKey,
 
 		Schema: map[string]*schema.Schema{
 			"user": {
@@ -37,7 +42,7 @@ type chefUserKey struct {
 	Key  chefc.AccessKey
 }
 
-func CreateUserKey(d *schema.ResourceData, meta interface{}) error {
+func CreateUserKey(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	c := meta.(*chefClient)
 
 	key, err := userKeyFromResourceData(d)
@@ -46,71 +51,97 @@ func CreateUserKey(d *schema.ResourceData, meta interface{}) error {
 	}
 
 	if _, err := c.Global.Users.AddKey(key.User, key.Key); err != nil {
-		return err
-	}
-
-	d.SetId(key.User + "+" + key.Key.Name)
-	return ReadUserKey(d, meta)
-}
-
-func UpdateUserKey(d *schema.ResourceData, meta interface{}) error {
-	c := meta.(*chefClient)
-
-	key, err := userKeyFromResourceData(d)
-	if err != nil {
-		return err
-	}
-
-	if _, err = c.Global.Users.UpdateKey(key.User, key.Key.Name, key.Key); err != nil {
-		return err
-	}
-
-	d.SetId(key.User + "+" + key.Key.Name)
-	return ReadUserKey(d, meta)
-}
-
-func ReadUserKey(d *schema.ResourceData, meta interface{}) error {
-	c := meta.(*chefClient)
-
-	key, err := userKeyFromResourceData(d)
-	if err != nil {
-		return err
-	}
-
-	k, err := c.Global.Users.GetKey(key.User, key.Key.Name)
-	if err != nil {
-		if errRes, ok := err.(*chefc.ErrorResponse); ok {
-			if errRes.Response.StatusCode == 404 {
-				d.SetId("")
-				return nil
-			}
-		} else {
-			return err
+		return diag.Diagnostics{
+			{
+				Severity:      diag.Error,
+				Summary:       "Error creating user key",
+				Detail:        fmt.Sprint(err),
+				AttributePath: cty.GetAttrPath("key_name"),
+			},
 		}
 	}
 
-	d.Set("user", key.User)
-	d.Set("key_name", k.Name)
-	d.Set("public_key", k.PublicKey)
-
-	return nil
+	d.SetId(key.User + "+" + key.Key.Name)
+	return ReadUserKey(ctx, d, meta)
 }
 
-func DeleteUserKey(d *schema.ResourceData, meta interface{}) error {
+func UpdateUserKey(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	c := meta.(*chefClient)
 
 	key, err := userKeyFromResourceData(d)
 	if err != nil {
 		return err
 	}
-	if _, err = c.Global.Users.DeleteKey(key.User, key.Key.Name); err == nil {
-		d.SetId("")
+
+	if _, err := c.Global.Users.UpdateKey(key.User, key.Key.Name, key.Key); err != nil {
+		return diag.Diagnostics{
+			{
+				Severity:      diag.Error,
+				Summary:       "Error updating user key",
+				Detail:        fmt.Sprint(err),
+				AttributePath: cty.GetAttrPath("key_name"),
+			},
+		}
 	}
 
-	return err
+	d.SetId(key.User + "+" + key.Key.Name)
+	return ReadUserKey(ctx, d, meta)
 }
 
-func userKeyFromResourceData(d *schema.ResourceData) (*chefUserKey, error) {
+func ReadUserKey(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	c := meta.(*chefClient)
+
+	key, err := userKeyFromResourceData(d)
+	if err != nil {
+		return err
+	}
+
+	if k, err := c.Global.Users.GetKey(key.User, key.Key.Name); err == nil {
+		d.Set("user", key.User)
+		d.Set("key_name", k.Name)
+		d.Set("public_key", k.PublicKey)
+	} else {
+		if errRes, ok := err.(*chefc.ErrorResponse); ok {
+			if errRes.Response.StatusCode == 404 {
+				d.SetId("")
+			}
+		} else {
+			return diag.Diagnostics{
+				{
+					Severity:      diag.Error,
+					Summary:       "Error reading user key",
+					Detail:        fmt.Sprint(err),
+					AttributePath: cty.GetAttrPath("key_name"),
+				},
+			}
+		}
+	}
+	return nil
+}
+
+func DeleteUserKey(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	c := meta.(*chefClient)
+
+	key, err := userKeyFromResourceData(d)
+	if err != nil {
+		return err
+	}
+	if _, err := c.Global.Users.DeleteKey(key.User, key.Key.Name); err == nil {
+		d.SetId("")
+		return nil
+	} else {
+		return diag.Diagnostics{
+			{
+				Severity:      diag.Error,
+				Summary:       "Error deleting user key",
+				Detail:        fmt.Sprint(err),
+				AttributePath: cty.GetAttrPath("key_name"),
+			},
+		}
+	}
+}
+
+func userKeyFromResourceData(d *schema.ResourceData) (*chefUserKey, diag.Diagnostics) {
 	key := &chefUserKey{
 		User: d.Get("user").(string),
 		Key: chefc.AccessKey{
